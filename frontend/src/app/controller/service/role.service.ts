1950f9d263e048135f4ce365890fd4914fbcd08a
m-elmouden <mohamed.elmouden@edu.uca.ma>
2022-12-03 23:09:12 +0100

    adding Role component

--- frontend/src/app/controller/service/role.service.ts
+++ frontend/src/app/controller/service/role.service.ts
@@ -1,9 +1,10 @@
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Role} from '../model/Role.model';
 import {BehaviorSubject, Observable} from 'rxjs';
 import {take} from 'rxjs/operators';
 import {environment} from '../../../environments/environment';
+import {User} from "../model/User.model";
 
 @Injectable({
     providedIn: 'root'
@@ -11,11 +12,13 @@ import {environment} from '../../../environments/environment';
 export class RoleService {
     public _role = new BehaviorSubject<string>('');
     public role$: Observable<string> = this._role.asObservable();
-    private readonly API = environment.apiUrl;
+    private  API = environment.apiUrl;
     selectedRoles: Role[];
 
     constructor(private http: HttpClient) {
-    }
+        this.role$.subscribe(role => {
+            this.API = environment.apiUrl +"roles/"+ role.toLowerCase() +"/";
+    })}
 
     _roles: Role[] = [];
     private _selectedRole : Role;
@@ -41,7 +44,7 @@ export class RoleService {
 
 
     async findAll() {
-        const roles = await this.http.get<Role[]>(this.API + "roles/").pipe(take(1)).toPromise();
+        const roles = await this.http.get<Role[]>(this.API).pipe(take(1)).toPromise();
         this._roles = roles;
     }
 
@@ -61,11 +64,22 @@ export class RoleService {
     }
 
     save(selectedRole: Role) {
-
+        this.http.post<User>(this.API +"save/", selectedRole).subscribe(user => {
+            this._roles = [...this._roles, selectedRole];
+        }, (error: HttpErrorResponse) => {
+            console.log(error.error)
+        })
     }
 
     update(selectedRole: Role) {
-
+        this.http.put<User>(this.API+"update/", selectedRole).subscribe(role => {
+            const index = this._roles.findIndex(roleToBeFound => role.id == roleToBeFound.id);
+            index > -1 ? this._roles[index] = selectedRole : false;
+            console.log("updated User")
+            console.log(selectedRole)
+        }, (error: HttpErrorResponse) => {
+            console.log(error.error)
+        });
     }
 
     delete(id: string) {
